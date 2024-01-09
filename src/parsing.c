5380b69ab99984c85a87234692904240517dc478
Elouan Mauduit <emauduit@bess-f2r4s19.clusters.42paris.fr>
2024-01-09 19:34:10 +0100

    push

--- src/parsing.c
+++ src/parsing.c
@@ -7,6 +7,9 @@ int atoi_parsing(char *str, int *index)
 
     divid = 1; 
     result = 0;
+    
+    if (*index > 0 && str[*index] != ' ')
+        display_error("LOOOOOOOOOOOL");
     while (str[*index] == ' ')
         (*index) ++;
     if (str[*index] == '-' || str[*index] == '+')
@@ -19,11 +22,11 @@ int atoi_parsing(char *str, int *index)
         display_error(NO_DIGIT_ERROR);
     while (ft_isdigit(str[*index]))
         result = (result * 10) + (str[(*index)++] - '0');
-    while (str[*index] == ' ')
-        (*index) ++;
+    // while (str[*index] == ' ')
+    //     (*index) ++;
     result = result * divid;
     if (result > INT_MAX || result < INT_MIN)
         display_error(INT_MAX_MIN_ERROR);
-    return (result * divid);
+    return (result);
 }   
 
